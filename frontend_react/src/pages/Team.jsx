39150ce0f324b73806ca25c25acf4d52da5f60ca
Pablo G <pablosgomez50@gmail.com>
2023-02-07 15:41:53 -0300

    FrontEnd: creacion de pagina Team

--- frontend_react/src/pages/Team.jsx
+++ frontend_react/src/pages/Team.jsx
@@ -1,11 +1,78 @@
-import { Box, Typography, useTheme } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import { DataGrid } from '@mui/x-data-grid';
 
+import { tokens } from '../theme';
+import { teamData } from "../fake_data/data";
+
+import Header from "../components/Header";
+
+
+const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+  ];
+
 const Team = () => {
+    
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
 
     return (
-        <Box>
-            Team
+        <Box m="20px">
+            <Header title="TEAM" subtitle="Managing the Team Members" />
+            <Box
+                m="40px 0 0 0"
+                height="75vh"
+                sx={{
+                    "& .MuiDataGrid-root": {
+                        border: "none",
+                    },
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none",
+                    },
+                    "& .name-column--cell": {
+                        color: colors.greenAccent[300],
+                    },
+                    "& .MuiDataGrid-columnHeaders": {
+                        backgroundColor: colors.blueAccent[700],
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: colors.primary[400],
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        borderTop: "none",
+                        backgroundColor: colors.blueAccent[700],
+                    },
+                    "& .MuiCheckbox-root": {
+                        color: `${colors.greenAccent[200]} !important`,
+                    },
+                }}
+            >
+                <DataGrid checkboxSelection rows={teamData} columns={columns} />
+            </Box>
         </Box>
     )
 }
